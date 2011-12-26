817e242d63cff498a3fecaea1dc6036bef4eec3a
Hana.WanTing <Hana.WanTing@gmail.com>
2011-12-26 05:06:21 +0000

    modified countback.xml

--- TrickyExpert/gen/com/diat/R.java
+++ TrickyExpert/gen/com/diat/R.java
@@ -10,7 +10,8 @@ package com.diat;
 public final class R {
     public static final class anim {
         public static final int alarm_animation=0x7f040000;
-        public static final int firefox_animation=0x7f040001;
+        public static final int banger_animation=0x7f040001;
+        public static final int firefox_animation=0x7f040002;
     }
     public static final class attr {
     }
@@ -21,86 +22,101 @@ public final class R {
         public static final int alarm_logo=0x7f020003;
         public static final int backgroud=0x7f020004;
         public static final int banger=0x7f020005;
-        public static final int bg1=0x7f020006;
-        public static final int bg2=0x7f020007;
-        public static final int bg_count=0x7f020008;
-        public static final int bomb=0x7f020009;
-        public static final int boo=0x7f02000a;
-        public static final int bowser=0x7f02000b;
-        public static final int brokenscreen=0x7f02000c;
-        public static final int brokenscreen_logo=0x7f02000d;
-        public static final int bullet1=0x7f02000e;
-        public static final int buttun_b=0x7f02000f;
-        public static final int cockroach=0x7f020010;
-        public static final int fart=0x7f020011;
-        public static final int ghost=0x7f020012;
-        public static final int icon=0x7f020013;
-        public static final int logo=0x7f020014;
+        public static final int banger0=0x7f020006;
+        public static final int banger1=0x7f020007;
+        public static final int bg1=0x7f020008;
+        public static final int bg2=0x7f020009;
+        public static final int bg_count=0x7f02000a;
+        public static final int bomb=0x7f02000b;
+        public static final int boo=0x7f02000c;
+        public static final int bowser=0x7f02000d;
+        public static final int brokenscreen=0x7f02000e;
+        public static final int brokenscreen_logo=0x7f02000f;
+        public static final int bullet1=0x7f020010;
+        public static final int buttun_b=0x7f020011;
+        public static final int cockroach=0x7f020012;
+        public static final int count=0x7f020013;
+        public static final int count1=0x7f020014;
+        public static final int count2=0x7f020015;
+        public static final int fart=0x7f020016;
+        public static final int ghost=0x7f020017;
+        public static final int icon=0x7f020018;
+        public static final int logo=0x7f020019;
     }
     public static final class id {
-        public static final int alarm=0x7f07001a;
+        public static final int alarm=0x7f07001e;
         public static final int alarm_anim=0x7f070004;
-        public static final int banger=0x7f070018;
-        public static final int brokenscreen=0x7f070017;
-        public static final int cockroach=0x7f070019;
-        public static final int count_fart_name=0x7f070007;
-        public static final int count_hint=0x7f070006;
-        public static final int countback=0x7f070005;
+        public static final int banger=0x7f07001c;
+        public static final int banger_anim=0x7f070008;
+        public static final int brokenscreen=0x7f07001b;
+        public static final int cockroach=0x7f07001d;
+        public static final int count_fart_name=0x7f07000b;
+        public static final int count_hint=0x7f07000a;
+        public static final int countback=0x7f070009;
         public static final int delay=0x7f070003;
-        public static final int delay_dialog=0x7f07000c;
-        public static final int delay_edit=0x7f07000e;
-        public static final int delay_label=0x7f07000d;
-        public static final int delay_unit=0x7f07000f;
-        public static final int fart=0x7f070016;
-        public static final int fart_anim=0x7f070013;
-        public static final int ghost=0x7f070015;
+        public static final int delay_dialog=0x7f070010;
+        public static final int delay_edit=0x7f070012;
+        public static final int delay_label=0x7f070011;
+        public static final int delay_unit=0x7f070013;
+        public static final int fart=0x7f07001a;
+        public static final int fart_anim=0x7f070017;
+        public static final int first_banger=0x7f070005;
+        public static final int ghost=0x7f070019;
         public static final int juicy_alarm=0x7f070001;
-        public static final int juicy_fart=0x7f070011;
-        public static final int logo=0x7f070014;
+        public static final int juicy_fart=0x7f070015;
+        public static final int logo=0x7f070018;
         public static final int long_alarm=0x7f070002;
-        public static final int long_fart=0x7f070012;
-        public static final int main_gridview=0x7f070008;
+        public static final int long_fart=0x7f070016;
+        public static final int main_gridview=0x7f07000c;
         public static final int normal_alarm=0x7f070000;
-        public static final int normal_fart=0x7f070010;
-        public static final int pause=0x7f07000a;
-        public static final int resume=0x7f07000b;
-        public static final int start=0x7f070009;
+        public static final int normal_fart=0x7f070014;
+        public static final int pause=0x7f07000e;
+        public static final int resume=0x7f07000f;
+        public static final int second_banger=0x7f070006;
+        public static final int start=0x7f07000d;
+        public static final int third_banger=0x7f070007;
     }
     public static final class layout {
         public static final int alarm=0x7f030000;
-        public static final int brokenscreen=0x7f030001;
-        public static final int countback=0x7f030002;
-        public static final int fart=0x7f030003;
-        public static final int fart1=0x7f030004;
-        public static final int fartdelay=0x7f030005;
-        public static final int initbrokenscreen=0x7f030006;
-        public static final int main=0x7f030007;
+        public static final int banger=0x7f030001;
+        public static final int brokenscreen=0x7f030002;
+        public static final int countback=0x7f030003;
+        public static final int fart=0x7f030004;
+        public static final int fart1=0x7f030005;
+        public static final int fartdelay=0x7f030006;
+        public static final int initbrokenscreen=0x7f030007;
+        public static final int main=0x7f030008;
     }
     public static final class raw {
         public static final int banger=0x7f050000;
         public static final int fastshoot=0x7f050001;
         public static final int fearless=0x7f050002;
         public static final int firecracker=0x7f050003;
-        public static final int froggy=0x7f050004;
-        public static final int juice_fart=0x7f050005;
-        public static final int juicy_alarm=0x7f050006;
-        public static final int ladystyle=0x7f050007;
-        public static final int long_alarm=0x7f050008;
-        public static final int long_fart=0x7f050009;
-        public static final int motorcycle=0x7f05000a;
-        public static final int normal_alarm=0x7f05000b;
-        public static final int normal_fart=0x7f05000c;
-        public static final int repressed=0x7f05000d;
+        public static final int first_banger=0x7f050004;
+        public static final int froggy=0x7f050005;
+        public static final int juice_fart=0x7f050006;
+        public static final int juicy_alarm=0x7f050007;
+        public static final int ladystyle=0x7f050008;
+        public static final int long_alarm=0x7f050009;
+        public static final int long_fart=0x7f05000a;
+        public static final int motorcycle=0x7f05000b;
+        public static final int normal_alarm=0x7f05000c;
+        public static final int normal_fart=0x7f05000d;
+        public static final int repressed=0x7f05000e;
+        public static final int second_banger=0x7f05000f;
+        public static final int third_banger=0x7f050010;
     }
     public static final class string {
         public static final int alarm=0x7f06000d;
         public static final int app_name=0x7f060000;
         public static final int back=0x7f060009;
+        public static final int banger=0x7f060014;
         public static final int brokenscreen=0x7f060013;
         public static final int cancel=0x7f060008;
         public static final int countback=0x7f060011;
         public static final int delay=0x7f060005;
         public static final int fart=0x7f060001;
+        public static final int first_banger=0x7f060015;
         public static final int juicy_alarm=0x7f060010;
         public static final int juicy_fart=0x7f06000c;
         public static final int long_alarm=0x7f06000f;
@@ -109,9 +125,11 @@ public final class R {
         public static final int normal_fart=0x7f06000a;
         public static final int pause=0x7f060003;
         public static final int resume=0x7f060004;
+        public static final int second_banger=0x7f060016;
         public static final int selfdefined_delay=0x7f060006;
         public static final int start=0x7f060002;
         public static final int submit=0x7f060007;
+        public static final int third_banger=0x7f060017;
         public static final int white=0x7f060012;
     }
 }
